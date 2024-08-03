637dfa39c4531677e92f1b18e86019164540fd03
Anjalisharma011 <122103601+Anjalisharma011@users.noreply.github.com>
2024-08-03 13:32:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the left over element/find-the-left-over-element.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int leftElement(vector<int> arr) {
+        // Your code goes here
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        if(n%2==0){
+            return arr[n/2-1];
+        }
+        else
+        return arr[n/2];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.leftElement(arr);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
